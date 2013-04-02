53c7953874b270cd8abdb69a0c23d3140f96b981
Hettomei <itsumo.sibyllin@gmail.com>
2013-04-02 17:59:07 +0200

    Foregn commit... + add an help message + do job only if no args to avoid mistake

--- pause.rb
+++ pause.rb
@@ -16,8 +16,10 @@ require 'sqlite3'
 
 class Pause
 
+  ENVIRONNMENT = "prod" # "prod" # "dev"
+
   def db
-    dtb_file = File.join(File.dirname(File.expand_path(__FILE__)), 'z_pause')
+    dtb_file = File.join(File.dirname(File.expand_path(__FILE__)), "z_pause_#{ENVIRONNMENT}")
     @db ||= SQLite3::Database.new( dtb_file )
   end
 
@@ -44,6 +46,7 @@ class Pause
 
   def start_pause
     start_time = Time.now
+    display_all_pause_of_this_day
     puts 'Début de la pause : ' + start_time.to_s
     db.execute( "insert into temp values ( ? )", start_time.to_i )
   end
@@ -68,16 +71,19 @@ class Pause
       else
         str.concat('  ')
       end
-       str.concat(Time.at(day[1].abs).utc.strftime("%H:%M:%S"))
-       puts str
+      str.concat(Time.at(day[1].abs).utc.strftime("%H:%M:%S"))
+      puts str
     end
     sum = db.execute("select sum(duration) from pauses where date(day, 'unixepoch') >= date('now') and date(day, 'unixepoch') <= date('now', '+1 day');")
-    unless sum.flatten.compact.empty?
-      puts "Durée total : " + Time.at(sum.flatten.first).utc.strftime("%H:%M:%S")
-    end
     if started?
-      puts 'Une pause est en cours, début : ' + Time.at(last_entry_time).strftime("%H:%M:%S")
-      puts "Durée : #{Time.at(Time.now - last_entry_time).utc.strftime("%H:%M:%S")}"
+      puts 'Pause en cours à : ' + Time.at(last_entry_time).strftime("%H:%M:%S") + " durée : #{Time.at(Time.now - last_entry_time).utc.strftime("%H:%M:%S")}"
+      unless sum.flatten.compact.empty?
+        puts "Durée total : " + Time.at((Time.now - last_entry_time) + sum.flatten.first).utc.strftime("%H:%M:%S")
+      end
+    else
+      unless sum.flatten.compact.empty?
+        puts "Durée total : " + Time.at(sum.flatten.first).utc.strftime("%H:%M:%S")
+      end
     end
   end
 
@@ -88,15 +94,34 @@ class Pause
   def add_pause_minutes(minutes)
     add_pause(minutes*60)
   end
+
+  def display_help
+    puts "ruby pause.rb <- start or stop the timer"
+    puts "ruby pause.rb show <- display all taken pause for this day"
+    puts "ruby pause.rb add x <- add an arbitrary pause. x is in minutes can be positiv or negative."
+    puts "------"
+    puts "I recommand to create an alias :"
+    puts "alias pause='ruby pause.rb'"
+  end
 end
 
 pause = Pause.new
+
 case ARGV[0]
+
 when 'show'
   pause.display_all_pause_of_this_day
 when 'add'
   pause.add_pause_minutes(ARGV[1].to_i)
   pause.display_all_pause_of_this_day
+when '-h'
+  pause.display_help
 else
-  pause.start_or_end_pause
+  if ARGV.count == 0
+    pause.start_or_end_pause
+  else
+    puts "ArgumentError"
+    pause.display_help
+  end
+
 end
